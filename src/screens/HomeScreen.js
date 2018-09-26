2670d85639b493d0f3f6838f606221f26669b8c9
Smile <xcxerxes@aliyun.com>
2018-09-26 17:25:10 +0800

    删除多余属性

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -57,8 +57,7 @@ class HomeScreen extends Component {
             <TabBar 
             {...props}
             style={{
-              backgroundColor: 'transparent',
-              color: '#444'
+              backgroundColor: 'transparent'
             }}
             labelStyle={{color: '#444'}}
             indicatorStyle={{backgroundColor: '#444'}}
