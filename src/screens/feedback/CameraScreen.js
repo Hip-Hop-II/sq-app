f02364ce4e6fdc00f294bb03cd298e127b9282a2
Smile <xcxerxes@aliyun.com>
2018-09-28 17:11:15 +0800

    删除;

--- src/screens/feedback/CameraScreen.js
+++ src/screens/feedback/CameraScreen.js
@@ -1,6 +1,6 @@
-import React, { PureComponent } from "react";
-import { Text, View, StyleSheet, StatusBar, TouchableOpacity } from "react-native";
-import { Camera, FileSystem } from "expo";
+import React, { PureComponent } from "react"
+import { Text, View, StyleSheet, StatusBar, TouchableOpacity } from "react-native"
+import { Camera, FileSystem } from "expo"
 import {Ionicons} from '@expo/vector-icons'
 
 export default class CameraScreen extends PureComponent {
@@ -20,7 +20,7 @@ export default class CameraScreen extends PureComponent {
       await FileSystem.moveAsync({
         from: photo.uri,
         to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`
-      });
+      })
     } catch (error) {
       throw error
     }
@@ -41,14 +41,14 @@ export default class CameraScreen extends PureComponent {
             "We need your permission to use your camera phone"
           }
           onGoogleVisionBarcodesDetected={({ barcodes }) => {
-            console.log(barcodes);
+            console.log(barcodes)
           }}
         />
         <TouchableOpacity style={styles.cameraButton} onPress={this.takePictureOnpress}>
           <Ionicons name="ios-camera" size={100} />
         </TouchableOpacity>
       </View>
-    );
+    )
   }
 }
 const styles = StyleSheet.create({
@@ -63,4 +63,4 @@ const styles = StyleSheet.create({
     bottom: 40,
     alignSelf: 'center'
   }
-});
+})
